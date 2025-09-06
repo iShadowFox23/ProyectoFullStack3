c74602f66398453755f7d747a94f61490b7404f7
Catalina-Kupfer <ca.kupfer@duocuc.cl>
2025-09-06 18:23:03 -0400

    Validaciones

--- js/login.js
+++ js/login.js
@@ -1,11 +1,36 @@
-const giroCarta=document.getElementById('giroCarta');
-const registerText=document.getElementById('registroTexto');
-const loginText=document.getElementById('InicioTexto');
+document.addEventListener('DOMContentLoaded', function(){
+    const registerForm = document.getElementById('register-form');
+    if(registerForm){
+        registerForm.addEventListener('submit', function(event){
+            const emailInput = document.getElementById('email');
+            const passwordInput = document.getElementById('password');
+            const confirmPasswordInput = confirmPasswordInput = document.getElementById('confirm-password');
+            const email = emailInput.value;
+            const password = passwordInput.value;
+            const confirmPassword = confirmPasswordInput.value;
 
-registerText.addEventListener('click',()=>{
-    giroCarta.style.transform='rotateY(180deg)'
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(email)) {
+                alert('El formato del correo no es válido. Intente nuevamente');
+                event.preventDefault(); 
+            }
+
+            if (password.length < 8) {
+                alert('La contraseña debe tener al menos 8 caracteres.');
+                event.preventDefault();
+                return;
+            }
+            
+            if (password !== confirmPassword) {
+                alert('Las contraseñas no coinciden.');
+                event.preventDefault();
+                return;
+            }
+
+        });
+    }
 });
 
-loginText.addEventListener('click',()=>{
-    giroCarta.style.transform='rotateY(0deg)'
-});
+
+
+
