5f7dfc9180c4c1e71d1831e3944b9d8b5831577a
Catalina-Kupfer <ca.kupfer@duocuc.cl>
2025-09-07 23:52:37 -0300

    Redireccion

--- js/login.js
+++ js/login.js
@@ -1,3 +1,37 @@
+
+document.addEventListener('DOMContentLoaded',function(){
+    const loginForm = document.getElementById('loginForm');
+    const emailInput = document.getElementById('emailInput');
+    const passwordInput = document.getElementById('passwordInput');
+    const errorMsg = document.getElementById('errorMsg');
+    const adminEmail = 'admin@magiceast.com';
+    const adminPassword = 'contraseñaaa';
+
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+        errorMsg.style.display= 'none';
+        errorMsg.textContent = '';
+        if(email===''||password===''){
+            errorMsg.textContent = 'Intente nuevamente';
+            errorMsg.style.display = 'block';
+            return;
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            errorMsg.textContent='El correo es inválido. Intente nuevamente';
+            errorMsg.style.display = 'block';
+            return;
+        }
+        if (email === "admin@magiceast.cl" && password === "contraseñaaa") {
+        setTimeout(() => {
+            window.location.href = "backoffice/backoffice.html"; 
+        }, 1000); 
+        return; 
+        }
+});
+
+
 document.addEventListener('DOMContentLoaded', function(){
     const registerForm = document.getElementById('register-form');
     if(registerForm){
@@ -30,7 +64,6 @@ document.addEventListener('DOMContentLoaded', function(){
         });
     }
 });
-
 
 
 
